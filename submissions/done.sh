a8290d69e39ad66436d7b210fcb94f858115e8e9
Lucas Lima <123587411+lucasdbr05@users.noreply.github.com>
2025-12-09 22:48:49 -0300

    Revise done.sh with completion steps
    Updated instructions for workshop completion.

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,2 +1,6 @@
-# Replit repository link: https://replit.com/@lucasg05/Programming-Lightning-Intro-to-Payment-Channels
+# When you finish the workshop:
+# 1. add a link to your replit fork in the next line
+# https://replit.com/@lucasg05/Programming-Lightning-Intro-to-Payment-Channels
+#
+# 2. change the FAIL string to PASS.
 echo "PASS"
